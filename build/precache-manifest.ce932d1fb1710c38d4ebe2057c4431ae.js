a7cf0dd1d10ff19b0c2eac1ba2e780b7f8a86c83
santoshd561 <santoshd561@gmail.com>
2021-03-17 10:48:56 +0530

    build

---
+++ build/precache-manifest.ce932d1fb1710c38d4ebe2057c4431ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3107cb0c17af2cef07640dff3cf7d427",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3dd34e279b947e4b140",
+    "url": "/static/css/2.84d6edd9.chunk.css"
+  },
+  {
+    "revision": "cee594c0aef2282dbd64",
+    "url": "/static/css/main.3abc1714.chunk.css"
+  },
+  {
+    "revision": "c3dd34e279b947e4b140",
+    "url": "/static/js/2.3439b725.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/static/js/2.3439b725.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cee594c0aef2282dbd64",
+    "url": "/static/js/main.2294a2c5.chunk.js"
+  },
+  {
+    "revision": "69bbd7c1381a265f29c9",
+    "url": "/static/js/runtime-main.fda65720.js"
+  }
+]);
